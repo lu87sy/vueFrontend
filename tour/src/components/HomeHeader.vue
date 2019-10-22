9dca35616207ab86d13c8603025e002214f5acb3
lu87sy <lu87sy@gmail.com>
2019-10-22 19:35:33 +0800

    add iconfont

--- tour/src/components/HomeHeader.vue
+++ tour/src/components/HomeHeader.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="test">
-        homeheader
+    <div class="header">
+        <div class="header-back">
+            <span class="iconfont">&#xe641;</span>
+        </div>
+        <div class="header-search">上海海昌海洋公园</div>
+        <div class="header-city">北京</div>
     </div>
 </template>
 
@@ -11,6 +15,23 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.test
-    color: red
+.header
+    line-height .88rem
+    text-align center
+    display flex
+    .header-back
+        float left
+        width .9rem
+    .header-search
+        flex 1
+        height .64rem
+        line-height .64rem
+        margin-top .12rem
+        margin-right .2rem
+        margin-left .2rem
+        border-radius .5rem
+        box-shadow 0 0 .01rem .02rem #005
+    .header-city
+        float right
+        width 1.4rem
 </style>
