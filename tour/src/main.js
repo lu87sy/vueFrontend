f7da689c7de815d766e7d900e113fb4e3144ec01
lu87sy <lu87sy@gmail.com>
2019-10-23 16:49:55 +0800

    commit homeswiper component

--- tour/src/main.js
+++ tour/src/main.js
@@ -5,9 +5,13 @@ import '@/assets/style/border.css'
 import '@/assets/style/reset.css'
 import faskClick from 'fastclick'
 import '@/assets/style/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 faskClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
--- tour/src/views/Home.vue
+++ tour/src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="home">
-    <home-header/>
+    <home-header class="home-header"/>
+    <home-swiper/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HomeHeader from '@/components/HomeHeader'
+import HomeSwiper from '@/components/HomeSwiper'
 
 export default {
-  name: 'Home',
-  components: {
-    HomeHeader
-  }
+    name: 'Home',
+    components: {
+        HomeHeader,
+        HomeSwiper
+    }
 }
 </script>
+
+<style lang="stylus" scoped>
+.home
+    position relative
+    .home-header
+        position absolute
+        top 0
+        left 0
+        z-index 100
+        width 100%
+</style>
